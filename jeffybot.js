362c1ead01665d6c966da9166b31a4a2f746ddec
Jeff Jones <jeff@beartooth.com>
2017-10-22 22:51:50 -0600

    fix incorrect song durations add skip and upnext commands update YoutubeView.js to use ES6 syntax

--- jeffybot.js
+++ jeffybot.js
@@ -11,16 +11,18 @@ function logErr(msg, err = false) {
 log("JeffyBot ver " + ver);
 checkEnv();
 
-const YT_API_KEY = process.env.YOUTUBE_API_KEY;
-const BOT_TOKEN = process.env.JEFFYPOO_BOT_TOKEN;
-const Discord = require("discord.js");
-const client = new Discord.Client();
-const fs = require('fs');
-const ytdl = require('ytdl-core');
-const request = require('superagent');
-const url = require('url');
+const YT_API_KEY = process.env.GOOGLE_API_KEY;
+const BOT_TOKEN  = process.env.JEFFYBOT_SECRET;
+const Discord    = require("discord.js");
+const client     = new Discord.Client();
+const fs         = require('fs');
+const ytdl       = require('ytdl-core');
+const request    = require('superagent');
+const url        = require('url');
+const moment     = require('moment');
+require("moment-duration-format");
 
-let YoutubeVideo = require('./model/YoutubeVideo.js');
+const YoutubeVideo = require('./model/YoutubeVideo.js');
 
 let boundTextChannel;
 let boundVoiceChannel;
@@ -46,29 +48,29 @@ client.login(BOT_TOKEN);
 
 
 const commands = {
-    "say": {
+    "say":    {
         argsDesc: "[message]",
-        desc: "Speak as JeffyBot himself.",
-        process: function (bot, msg, args) {
+        desc:     "Speak as JeffyBot himself.",
+        process:  function (bot, msg, args) {
             log(`say: '${args}'`);
             if (msg.deletable) {
                 msg.delete().then(function (deleted) {
                     log(`Deleted orig msg: '${deleted.content}'`);
                 }).catch(console.error)
             }
-            msg.channel.sendMessage(args);
+            msg.channel.send(args);
         }
     },
-    "play": {
+    "play":   {
         argsDesc: "[artist and/or song name]",
-        desc: "Searches youtube for a song and plays it.",
-        process: function (bot, msg, args) {
+        desc:     "Searches youtube for a song and plays it.",
+        process:  function (bot, msg, args) {
             log(`play: '${args.trim()}'`);
-            if (boundTextChannel != msg.channel) {
+            if (boundTextChannel !== msg.channel) {
                 log(`Binding text channel to '${msg.channel.name}'`);
                 boundTextChannel = msg.channel;
             }
-            if (boundVoiceChannel && msg.member.voiceChannel != boundVoiceChannel) {
+            if (boundVoiceChannel && msg.member.voiceChannel !== boundVoiceChannel) {
                 msg.reply(`I'm already playing music in '${boundVoiceChannel.name}'`);
                 return;
             } else if (boundVoiceChannel) {
@@ -89,10 +91,10 @@ const commands = {
 
         }
     },
-    "stop": {
+    "stop":   {
         argsDesc: false,
-        desc: "Stops any playing music.",
-        process: function (bot, msg, args) {
+        desc:     "Stops any playing music.",
+        process:  function (bot, msg, args) {
             log('stop');
             if (voiceStreamDispatcher) {
                 msg.reply('Stopping...');
@@ -101,20 +103,44 @@ const commands = {
             leaveVoiceChannel();
         }
     },
-    "pause": {
+    "pause":  {
         argsDesc: false,
-        desc: "Pauses any playing music (resume by calling !resume)",
-        process: function (bot, msg, args) {
+        desc:     "Pauses any playing music (resume by calling !resume)",
+        process:  function (bot, msg, args) {
             log('pause');
             pausePlayback(msg);
         }
     },
     "resume": {
         argsDesc: false,
-        desc: "Resumes paused music.",
-        process: function (bot, msg, args) {
+        desc:     "Resumes paused music.",
+        process:  function (bot, msg, args) {
             log('resume');
             resumePlayback(msg);
+        }
+    },
+    "skip":   {
+        argsDesc: false,
+        desc:     "Skips the current track.",
+        process:  function (bot, msg, args) {
+            log('skip');
+            skipCurrent(msg)
+        }
+    },
+    "upnext": {
+        argsDesc: false,
+        desc:     "Shows what song is up next.",
+        process:  function (bot, msg, args) {
+            log('upnext');
+            if (playQueue.length > 0) {
+                const next = playQueue[0];
+                msg.reply(`Next song is...\n${getMessageFriendlyVideoDesc(next)}`);
+                return
+            }
+            if (voiceStreamDispatcher && playQueue.length === 0) {
+                msg.reply("This is the last song.");
+                return
+            }
         }
     }
 };
@@ -123,8 +149,8 @@ function searchAndQueue(bot, msg, args, conn) {
     const searchURL = getYoutubeSearchURL(args);
     msg.reply('Searching...');
     request(searchURL, (err, resp) => {
-        if (!err && resp.statusCode == 200) {
-            if (resp.body.items.length == 0) {
+        if (!err && resp.statusCode === 200) {
+            if (resp.body.items.length === 0) {
                 msg.reply(`No videos matching '${args.trim()}'`);
                 return;
             }
@@ -162,21 +188,26 @@ function getVideoInfo(url, cb) {
 
 function playNext() {
     let next = playQueue.shift();
-    if (next) {
-        log(`playNext(): preparing track:\n${next.logString()}`);
-        const title = next.title();
-        const author = next.author();
-        const length = next.length();
-        const link = next.link();
-        const stream = ytdl.downloadFromInfo(next.info, {audioonly: true});
-        voiceStreamDispatcher = boundVoiceChannel.connection.playStream(stream);
-        voiceStreamDispatcher.once('end', () => {
-            log(`track '${title}' ended`);
-            voiceStreamDispatcher = false;
-            playNext();
-        });
-        boundTextChannel.sendMessage(`\n**Now Playing** : ${title}\n**Length** : ${length}\n**Uploader** : ${author}\n**Link** : ${link}`);
+    if (!next) {
+        killStreamDispatcher();
+        return;
     }
+    log(`playNext(): preparing track:\n${next.debugDescription}`);
+    const stream          = ytdl(next.link, {filter: 'audioonly'});
+    voiceStreamDispatcher = boundVoiceChannel.connection.playStream(stream);
+    voiceStreamDispatcher.on('end', (reason) => {
+        log(`track ended: ${reason || 'no reason'}`);
+        setTimeout(() => {
+            playNext()
+        }, 1000)
+    });
+    voiceStreamDispatcher.on('error', (err) => {
+        logErr('error occurred streaming song', err)
+        setTimeout(() => {
+            playNext()
+        }, 1000)
+    });
+    boundTextChannel.send(`Changing song...\n${getMessageFriendlyVideoDesc(next)}`)
 }
 
 function pausePlayback(msg) {
@@ -202,10 +233,30 @@ function resumePlayback(msg) {
     }
 }
 
+function skipCurrent(msg) {
+    if (voiceStreamDispatcher) {
+        msg.reply('Skipping...');
+        killStreamDispatcher()
+    }
+}
+
+function killStreamDispatcher() {
+    if (voiceStreamDispatcher) {
+        voiceStreamDispatcher.end();
+        voiceStreamDispatcher = false;
+    }
+}
+
 function getYoutubeSearchURL(query) {
     return `https://www.googleapis.com/youtube/v3/search?part=snippet&q=${escape(query.trim())}&key=${YT_API_KEY}`;
 }
 
+function getMessageFriendlyVideoDesc(video) {
+    let title    = String(video.title);
+    let duration = moment.duration(parseInt(video.length), 'seconds').format('mm:ss');
+    let link     = String(video.link);
+    return `**Title** : ${title}\n**Duration** : ${duration}\n**Link** : ${link}`
+}
 
 function leaveVoiceChannel() {
     if (boundVoiceChannel) {
@@ -242,22 +293,21 @@ function printCommands(msg) {
     for (let cmd in commands) {
         if (!commands[cmd]) continue;
         const cmdBody = commands[cmd];
-        var argsDesc = cmdBody.argsDesc || '';
+        var argsDesc  = cmdBody.argsDesc || '';
         cmdsString += `\n**!${cmd} ${argsDesc}** : ${cmdBody.desc}\n`;
     }
     msg.reply(cmdsString);
 }
 
 function checkEnv() {
-    if (!process.env.JEFFYPOO_BOT_TOKEN) {
+    if (!process.env.JEFFYBOT_SECRET) {
         logErr('ENV var JEFFYPOO_BOT_TOKEN not defined.');
         process.exit(-2);
     }
-    if (!process.env.YOUTUBE_API_KEY) {
-        logErr('ENV var YOUTUBE_API_KEY not defined.');
+    if (!process.env.GOOGLE_API_KEY) {
+        logErr('ENV var GOOGLE_API_KEY not defined.');
         process.exit(-2);
     }
 }
-
 
 
--- model/YoutubeVideo.js
+++ model/YoutubeVideo.js
@@ -1,29 +1,58 @@
 
-var moment = require('moment');
+// var moment = require('moment');
 
-module.exports = YoutubeVideo = function(url, info) {
-    this.url = url;
-    this.info = info;
-};
+// module.exports = YoutubeVideo = function(url, info) {
+//     this.url = url;
+//     this.info = info;
+// };
+//
+// YoutubeVideo.prototype.title = function () {
+//     return this.info.title;
+// };
+//
+// YoutubeVideo.prototype.author = function () {
+//     return this.info.author;
+// };
+//
+// YoutubeVideo.prototype.length = function () {
+//     var secs = this.info.lengthSeconds || this.info.length_seconds;
+//     console.log(`length(): secs = ${secs}`);
+//     return ;
+// };
+//
+// YoutubeVideo.prototype.link = function () {
+//     return this.url;
+// };
+//
+// YoutubeVideo.prototype.logString = function () {
+//     return `URL=${this.url}\nTitle=${this.title()}\nLength=${this.length()}`;
+// };
 
-YoutubeVideo.prototype.title = function () {
-    return this.info.title;
-};
+module.exports = class YoutubeVideo {
 
-YoutubeVideo.prototype.author = function () {
-    return this.info.author;
-};
+    constructor(url, info) {
+        this.url = url;
+        this.info = info;
+    }
 
-YoutubeVideo.prototype.length = function () {
-    var secs = this.info.lengthSeconds || this.info.length_seconds;
-    console.log(`length(): secs = ${secs}`);
-    return moment().seconds(secs).format('mm:ss');
-};
+    get title() {
+        return this.info.title;
+    }
 
-YoutubeVideo.prototype.link = function () {
-    return this.url;
-};
+    get author() {
+        return this.info.author
+    }
 
-YoutubeVideo.prototype.logString = function () {
-    return `URL=${this.url}\nTitle=${this.title()}\nLength=${this.length()}`;
+    get length() {
+        return this.info.lengthSeconds || this.info.length_seconds;
+    }
+
+    get link() {
+        return this.url
+    }
+
+    get debugDescription() {
+        return `URL=${this.url}\nTitle=${this.title}\nLength=${this.length}`;
+    }
+
 };
